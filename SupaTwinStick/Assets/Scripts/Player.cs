e715d320fe73897955770b887f0221737d7173fd
loic <loic1.renaud@hotmail.fr>
2016-10-01 22:10:52 -0400

    Player LookAt mouse

--- SupaTwinStick/Assets/Scripts/Player.cs
+++ SupaTwinStick/Assets/Scripts/Player.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour {
 
-	// Use this for initialization    
-	void Start () {
-	
-	}
+    public float moveSpeed = 5;
+
+    PlayerController myPlayerController;
+    Camera viewCamera;
+
+    // Use this for initialization    
+    void Start () {
+        myPlayerController = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        /*Manage the movement of the player */
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        myPlayerController.Move(moveVelocity);
+
+        /* Manage the camera to look at the mouse cursor */
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane myPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(myPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            myPlayerController.LookAt(point);
+        }
+    }
 }
--- SupaTwinStick/Assets/Scripts/PlayerController.cs
+++ SupaTwinStick/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody body;
+    // Use this for initialization
+    void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void Move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 correctedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(correctedPoint);
+    }
+    // Update is called once per frame
+    //Boucle de jeu
+    public void FixedUpdate()
+    {
+        body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
+    }
+
 }
