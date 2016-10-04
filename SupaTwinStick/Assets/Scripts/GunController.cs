f27b623c4149f6420b595d5df8549bb441390482
Paul Ferrand <paulferrand@hotmail.fr>
2016-10-03 21:21:58 -0400

    Equipement premier jet avec des presets pour armes

---
+++ SupaTwinStick/Assets/Scripts/GunController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	public Transform gunHold;
+	public Gun startGun;
+	Gun equippedGun;
+
+	void Start() {
+		if (startGun != null) {
+			EquipGun(startGun);
+		}
+	}
+
+	//Se munir l'arme
+	public void EquipGun(Gun gunToEquip){
+		if (equippedGun != null) {
+			Destroy (equippedGun.gameObject);
+		}
+		equippedGun = Instantiate (gunToEquip, gunHold.position, gunHold.rotation) as Gun;
+		equippedGun.transform.parent = gunHold;
+	}
+}
